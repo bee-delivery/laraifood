75acda78c5d10d131a301f8003849039075df15d
flads <flads.me@gmail.com>
2021-11-24 11:48:05 -0300

    feat: get order details

--- src/Functions/Order.php
+++ src/Functions/Order.php
@@ -3,26 +3,33 @@
 namespace BeeDelivery\LaraiFood\Functions;
 
 use BeeDelivery\LaraiFood\Connection;
+use GuzzleHttp\Client;
 
 class Order
 {
-    protected $base_uri;
     protected $accessToken;
+    protected $client;
 
     public function __construct($accessToken)
     {
         $this->accessToken = $accessToken;
-        $this->base_uri = config('laraifood.base_uri');
+
+        $this->client = new Client([
+            'base_uri' => config('laraifood.base_uri'),
+        ]);
     }
 
+    /**
+     * State changes and other kinds of notifications related to orders on the platform.
+     *
+     * @param string $groups
+     * @param string $types
+     * @return array
+     */
     public function eventsPolling($groups = null, $types = null)
     {
-        $client = new \GuzzleHttp\Client([
-            'base_uri' => $this->base_uri,
-        ]);
-
         try {
-            $response = $client->request('GET', "order/v1.0/events:polling", [
+            $response = $this->client->request('GET', "order/v1.0/events:polling", [
                 'allow_redirects' => false,
                 'headers' => [
                     'Authorization' => 'Bearer ' . $this->accessToken,
@@ -44,7 +51,34 @@ class Order
                 'response' => $e->getMessage()
             ];
         }
-
     }
 
+    /**
+     * Full information on the order (items, payment, delivery information, etc.).
+     *
+     * @param uuid $orderId
+     * @return array
+     */
+    public function details($orderId)
+    {
+        try {
+            $response = $this->client->request('GET', "order/v1.0/orders/$orderId", [
+                'allow_redirects' => false,
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->accessToken,
+                ]
+            ]);
+
+            return [
+                'code' => $response->getStatusCode(),
+                'response' => json_decode($response->getBody(), true)
+            ];
+
+        } catch (\Exception $e) {
+            return [
+                'code' => $e->getCode(),
+                'response' => $e->getMessage()
+            ];
+        }
+    }
 }
